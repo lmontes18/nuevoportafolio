046f43e8170484f14f578fa8f00885d0eda90b6a
lmontes18 <lesy_montesz40@unicah.edu>
2024-11-01 00:48:07 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import * as glob from "glob";
+import path, { resolve } from "node:path";
+import htmlPurge from 'vite-plugin-purgecss';
+
+const getHtmlEntries = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync('./**/*.html', { ignore:['./dist/**','./node_modules/**']}).map(file=>[
+                file.slice(0, file.length - path.extname(file).length),
+                resolve(__dirname, file)
+            ])
+        ]
+    )
+}
+/*
+{
+    nombreArchiv: rutaDeArchivo,
+    nombreArchiv2: rutaDeArchivo2,
+}
+*/
+
+export default defineConfig(
+    {
+        appType: 'mpa',
+        base: "/2portafolio/",
+        build: {
+            rollupOptions: {
+                input: getHtmlEntries()
+            }
+        },
+        plugins: [
+            htmlPurge({}),
+        ]
+    }
+);
